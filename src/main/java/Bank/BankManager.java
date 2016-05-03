720c8784e9814dda8b85996bb51408e9d9e8c330
Finn Masurat <finn.masurat@hotmail.de>
2016-05-03 14:17:43 +0200

    bankService wip

---
+++ src/main/java/Bank/BankManager.java
@@ -0,0 +1,8 @@
+package Bank;
+
+public class BankManager {
+
+
+
+
+}
--- src/main/java/Bank/BankService.java
+++ src/main/java/Bank/BankService.java
@@ -1,9 +1,40 @@
 package Bank;
 
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static spark.Spark.before;
+import static spark.Spark.post;
+
 public class BankService {
 
     public void main(String[] args) {
 
+        BankManager bankManager = new BankManager();
+
+        before(((request, response) -> response.type("application/json")));
+        before(((request, response) -> {
+
+            if (!request.contentType().equals("application/json")) {
+                response.status(HTTP_BAD_REQUEST);
+                return;
+            }
+
+            response.header("Description", "An BankService for RESTopoly");
+            response.type("application/json");
+        }
+        ));
+
+
+        post("/banks/:gameid/players", (request, response) -> {
+            try {
+          //      return Tools.Helper.dataToJson(bankManager.createNewBankAccount(request.params(":gameid")));
+            } catch (Exception e) {
+                if (e instanceof Exception) {
+                    response.status(HTTP_BAD_REQUEST);
+                }
+            }
+            return "";
+        });
+
     }
-    
+
 }
