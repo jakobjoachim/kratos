36a40e2caba6714b522b625f9ffd7cfc932c9555
Jakob Joachim <jak.joachim@gmail.com>
2016-05-09 15:31:15 +0200

    payload done

--- src/main/java/Game/GameService.java
+++ src/main/java/Game/GameService.java
@@ -1,6 +1,12 @@
 package Game;
 
+import Exceptions.UserAlreadyExistsException;
+import User.UserPayload;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * Created by jakob on 09/05/16.
@@ -21,6 +27,28 @@ public class GameService {
             return model.getAllGames();
         });
 
-        
+        post("/games", (request, response) -> {
+            try{
+                ObjectMapper mapper = new ObjectMapper();
+                GamePayload creation = mapper.readValue(request.body(), GamePayload.class);
+                if (!(creation.isValid())){
+                    response.status(HTTP_BAD_REQUEST);
+                    return "";
+                }
+                String createdUrl = model.createUser(creation.getName(), creation.getUri());
+                response.status(OK);
+                response.type("application/json");
+                return createdUrl;
+            }catch(Exception e){
+                if (e instanceof UserAlreadyExistsException) {
+                    response.status(UNPROCESSABLE_ENTITY);
+                }
+                if (e instanceof JsonParseException) {
+                    response.status(HTTP_BAD_REQUEST);
+                }
+                return "";
+            }
+        });
+
     }
 }
