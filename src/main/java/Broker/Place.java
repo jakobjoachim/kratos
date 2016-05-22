28184c8b59afbf2a8ede4c98949a3fe04143f851
Jakob Joachim <jak.joachim@gmail.com>
2016-05-22 22:58:09 +0200

    ground work, place finished

---
+++ src/main/java/Broker/Place.java
@@ -0,0 +1,11 @@
+package Broker;
+
+import lombok.Data;
+
+@Data
+public class Place {
+    private String id;
+    private String type;
+    private String owner;
+    private boolean hypothecarycredit;
+}
