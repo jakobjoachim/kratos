9ac4ab70194ddddb5293b65032a09748f1892d47
Jakob Joachim <jak.joachim@gmail.com>
2016-05-24 22:49:07 +0200

    feature almost last part requires finished /bank to be continued

--- src/main/java/Broker/BrokerModel.java
+++ src/main/java/Broker/BrokerModel.java
@@ -1,8 +1,6 @@
 package Broker;
 
-import Exceptions.GameAlreadyExistsException;
-import Exceptions.GameDoesNotExistException;
-import Exceptions.PlaceAlreadyExistException;
+import Exceptions.*;
 import Tools.Helper;
 
 import java.util.HashMap;
@@ -31,6 +29,100 @@ public class BrokerModel {
                 return Helper.dataToJson(place);
             } else {
                 throw new PlaceAlreadyExistException();
+            }
+        } else {
+            throw new GameDoesNotExistException();
+        }
+    }
+
+    public String buyPlace(String placeId, String gameId, String playerUri, String placeType) throws Exception {
+        if (brokerMap.containsKey(gameId)) {
+            if (brokerMap.get(gameId).getPlaces().keySet().contains(placeId)) {
+                Place place = brokerMap.get(gameId).getPlaces().get(placeId);
+                if (place.getOwner() == null) {
+                    //TODO: Notify Bank and make sure place only gets sold if transaction is complete
+                    place.setOwner(playerUri);
+                    return Helper.dataToJson(place);
+                } else {
+                    throw new PlaceAlreadySoldException();
+                }
+            } else {
+                throw new PlaceDoesNotExistException();
+            }
+        } else {
+            throw new GameDoesNotExistException();
+        }
+    }
+
+    public String tradePlace(String placeId, String gameId, String playerUri) throws Exception{
+        if (brokerMap.containsKey(gameId)) {
+            if (brokerMap.get(gameId).getPlaces().keySet().contains(placeId)) {
+                Place place = brokerMap.get(gameId).getPlaces().get(placeId);
+                if (!(place.getOwner() == null)) {
+                    place.setOwner(playerUri);
+                    return Helper.dataToJson(place);
+                } else {
+                    throw new PlaceNotSoldException();
+                }
+            } else {
+                throw new PlaceDoesNotExistException();
+            }
+        } else {
+            throw new GameDoesNotExistException();
+        }
+    }
+
+    public String takeHypothecarycredit(String placeId, String gameId) throws Exception {
+        if (brokerMap.containsKey(gameId)) {
+            if (brokerMap.get(gameId).getPlaces().keySet().contains(placeId)) {
+                Place place = brokerMap.get(gameId).getPlaces().get(placeId);
+                if (!(place.isHypothecarycredit())) {
+                    //TODO: Notify Bank and make sure place is only Hypothecarycredited if transaction is successful
+                    place.setHypothecarycredit(true);
+                    return Helper.dataToJson(place);
+                } else {
+                    throw new AlreadyHasAHypothecarycreditException();
+                }
+            } else {
+                throw new PlaceDoesNotExistException();
+            }
+        } else {
+            throw new GameDoesNotExistException();
+        }
+    }
+
+    public String payHypothecarycredit(String placeId, String gameId) throws Exception {
+        if (brokerMap.containsKey(gameId)) {
+            if (brokerMap.get(gameId).getPlaces().keySet().contains(placeId)) {
+                Place place = brokerMap.get(gameId).getPlaces().get(placeId);
+                if (place.isHypothecarycredit()) {
+                    //TODO: Notify Bank and make sure place is only anti Hypothecarycredited if transaction is successful
+                    place.setHypothecarycredit(false);
+                    return Helper.dataToJson(place);
+                } else {
+                    throw new NoHypothecarycreditOnThePlaceException();
+                }
+            } else {
+                throw new PlaceDoesNotExistException();
+            }
+        } else {
+            throw new GameDoesNotExistException();
+        }
+    }
+
+    public String visitPlace(String placeId, String gameId, String playerUri, String placeType) throws Exception{
+        if (brokerMap.containsKey(gameId)) {
+            if (brokerMap.get(gameId).getPlaces().keySet().contains(placeId)) {
+                Place place = brokerMap.get(gameId).getPlaces().get(placeId);
+                if (!(place.getOwner() == null)) {
+                    String playerToPay = place.getOwner();
+                    //TODO: pay owner depending on the placeType??
+                    return Helper.dataToJson(place);
+                } else {
+                    throw new PlaceNotSoldException();
+                }
+            } else {
+                throw new PlaceDoesNotExistException();
             }
         } else {
             throw new GameDoesNotExistException();
