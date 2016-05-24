dc527fb4b7dfdf684e5c83464653acd43fa8299f
Jakob Joachim <jak.joachim@gmail.com>
2016-05-24 22:49:07 +0200

    feature almost last part requires finished /bank to be continued

--- src/main/java/Broker/Broker.java
+++ src/main/java/Broker/Broker.java
@@ -2,7 +2,6 @@ package Broker;
 
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 @Data
---
+++ src/main/java/Exceptions/NoHypothecarycreditOnThePlaceException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class NoHypothecarycreditOnThePlaceException extends Exception{
+}
