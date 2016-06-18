6c4ab1e4ba6371e42031df907ff0d08ae5d55afd
eneskaya <eneskaya@me.com>
2016-06-18 12:16:09 +0200

    Made dice service work

--- src/main/java/Dice/DiceService.java
+++ src/main/java/Dice/DiceService.java
@@ -1,9 +1,15 @@
 package Dice;
 
 import Enums.ServiceType;
+import Exceptions.BadDicePayloadException;
 import Tools.Helper;
 import Tools.YellowService;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static spark.Spark.*;
 
@@ -14,9 +20,23 @@ public class DiceService {
         before((request, response) -> response.header("Desciption", "Gives you a single dice roll"));
 
         get("/dice", (request, response) -> {
+
+            if (request.queryMap().get("player").value() == null
+                    || request.queryMap().get("game").value() == null) {
+
+                throw new BadDicePayloadException();
+            }
+
             createEvent(request.queryMap().get("player").value(), request.queryMap().get("game").value());
+
             response.header("Content-Type", "application/json");
             return randomDice();
+        });
+
+        exception(BadDicePayloadException.class, (exception, request, response) -> {
+            response.status(422);
+            response.header("Content-Type", "application/json");
+            response.body("{ \"error\": \"game and/or player missing\", \"status\": 422 }");
         });
 
     }
@@ -37,13 +57,15 @@ public class DiceService {
         dicePayload.setPlayer(player);
 
         try {
-            Unirest.post(eventCreationUrl)
+
+            URL url = new URL(eventCreationUrl);
+            Unirest.post(url.toString())
                     .header("Content-Type", "application/json")
                     .body(Helper.dataToJson(dicePayload))
-                    .asJson();
+                    .asString();
 
         } catch (Exception e) {
-            System.out.print(e.getMessage());
+            e.printStackTrace();
         }
 
     }
